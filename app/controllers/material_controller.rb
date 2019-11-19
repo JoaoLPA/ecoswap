ae82305ad597a45c4b43669dbaa7c2be751de9cd
Debora Alves <foonatic@gmail.com>
2019-11-19 16:05:34 -0300

    material_new completed

--- app/controllers/material_controller.rb
+++ app/controllers/material_controller.rb
@@ -6,4 +6,22 @@
   def show
     @material = Material.find(params[:id])
   end
+
+  def new
+    @material = Material.new
+  end
+
+  def create
+    raise
+    @material = Material.new(material_params)
+    raise
+
+  end
+
+
+  private
+
+  def material_params
+    params.require(:material).permit(:description, :price, :amount, :location, :available, :user_id)
+  end
 end
